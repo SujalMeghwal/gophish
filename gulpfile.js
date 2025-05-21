f40ae852ac9af1378d6ba1718c8d6737e3c896be
Sujal Meghwal <87793589+SujalMeghwal@users.noreply.github.com>
2025-05-21 08:11:11 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,117 @@
-/*
- * gulpfile.js
- *
- * Description: The Gophish gulpfile
+/**
+ * Improved Gophish Gulpfile
+ * Handles: Vendor JS, App Scripts, and Stylesheets
  */
 
-var gulp = require('gulp'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify-es').default,
-    cleanCSS = require('gulp-clean-css'),
-    babel = require('gulp-babel'),
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
 
-    js_directory = 'static/js/src/',
-    css_directory = 'static/css/',
-    vendor_directory = js_directory + 'vendor/',
-    app_directory = js_directory + 'app/',
-    dest_js_directory = 'static/js/dist/',
-    dest_css_directory = 'static/css/dist/';
+// === Directory Paths ===
+const paths = {
+    js: {
+        src: 'static/js/src/',
+        vendor: 'static/js/src/vendor/',
+        app: 'static/js/src/app/',
+        dest: 'static/js/dist/',
+    },
+    css: {
+        src: 'static/css/',
+        dest: 'static/css/dist/',
+    }
+};
 
-vendorjs = function () {
-    return gulp.src([
-            vendor_directory + 'jquery.js',
-            vendor_directory + 'bootstrap.min.js',
-            vendor_directory + 'moment.min.js',
-            vendor_directory + 'papaparse.min.js',
-            vendor_directory + 'd3.min.js',
-            vendor_directory + 'topojson.min.js',
-            vendor_directory + 'datamaps.min.js',
-            vendor_directory + 'jquery.dataTables.min.js',
-            vendor_directory + 'dataTables.bootstrap.js',
-            vendor_directory + 'datetime-moment.js',
-            vendor_directory + 'jquery.ui.widget.js',
-            vendor_directory + 'jquery.fileupload.js',
-            vendor_directory + 'jquery.iframe-transport.js',
-            vendor_directory + 'sweetalert2.min.js',
-            vendor_directory + 'bootstrap-datetime.js',
-            vendor_directory + 'select2.min.js',
-            vendor_directory + 'core.min.js',
-            vendor_directory + 'highcharts.js',
-            vendor_directory + 'ua-parser.min.js'
-        ])
-        .pipe(concat('vendor.js'))
-        .pipe(rename({
-            suffix: '.min'
-        }))
+// === Vendor JS Task ===
+function vendorJS() {
+    const files = [
+        'jquery.js',
+        'bootstrap.min.js',
+        'moment.min.js',
+        'papaparse.min.js',
+        'd3.min.js',
+        'topojson.min.js',
+        'datamaps.min.js',
+        'jquery.dataTables.min.js',
+        'dataTables.bootstrap.js',
+        'datetime-moment.js',
+        'jquery.ui.widget.js',
+        'jquery.fileupload.js',
+        'jquery.iframe-transport.js',
+        'sweetalert2.min.js',
+        'bootstrap-datetime.js',
+        'select2.min.js',
+        'core.min.js',
+        'highcharts.js',
+        'ua-parser.min.js'
+    ].map(file => paths.js.vendor + file);
+
+    return gulp.src(files)
+        .pipe(plumber())
+        .pipe(concat('vendor.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest(dest_js_directory));
+        .pipe(gulp.dest(paths.js.dest));
 }
 
-scripts = function () {
-    // Gophish app files - non-ES6
-    return gulp.src([
-            app_directory + 'autocomplete.js',
-            app_directory + 'campaign_results.js',
-            app_directory + 'campaigns.js',
-            app_directory + 'dashboard.js',
-            app_directory + 'groups.js',
-            app_directory + 'landing_pages.js',
-            app_directory + 'sending_profiles.js',
-            app_directory + 'settings.js',
-            app_directory + 'templates.js',
-            app_directory + 'gophish.js',
-            app_directory + 'users.js',
-            app_directory + 'webhooks.js',
-            app_directory + 'passwords.js'
-        ])
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(uglify().on('error', function (e) {
-            console.log(e);
-        }))
-        .pipe(gulp.dest(dest_js_directory + 'app/'));
+.pipe(uglify())
+// === App JS Task ===
+function appScripts() {
+    const files = [
+        'autocomplete.js',
+        'campaign_results.js',
+        'campaigns.js',
+        'dashboard.js',
+        'groups.js',
+        'landing_pages.js',
+        'sending_profiles.js',
+        'settings.js',
+        'templates.js',
+        'gophish.js',
+        'users.js',
+        'webhooks.js',
+        'passwords.js'
+    ].map(file => paths.js.app + file);
+
+    return gulp.src(files)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.js.dest + 'app/'));
 }
 
-styles = function () {
-    return gulp.src([
-            css_directory + 'bootstrap.min.css',
-            css_directory + 'main.css',
-            css_directory + 'dashboard.css',
-            css_directory + 'flat-ui.css',
-            css_directory + 'dataTables.bootstrap.css',
-            css_directory + 'font-awesome.min.css',
-            css_directory + 'chartist.min.css',
-            css_directory + 'bootstrap-datetime.css',
-            css_directory + 'checkbox.css',
-            css_directory + 'sweetalert2.min.css',
-            css_directory + 'select2.min.css',
-            css_directory + 'select2-bootstrap.min.css',
-        ])
-        .pipe(cleanCSS({
-            compatibilty: 'ie9'
-        }))
-        .pipe(concat('gophish.css'))
-        .pipe(gulp.dest(dest_css_directory));
+// === CSS Task ===
+function styles() {
+    const files = [
+        'bootstrap.min.css',
+        'main.css',
+        'dashboard.css',
+        'flat-ui.css',
+        'dataTables.bootstrap.css',
+        'font-awesome.min.css',
+        'chartist.min.css',
+        'bootstrap-datetime.css',
+        'checkbox.css',
+        'sweetalert2.min.css',
+        'select2.min.css',
+        'select2-bootstrap.min.css'
+    ].map(file => paths.css.src + file);
+
+    return gulp.src(files)
+        .pipe(plumber())
+        .pipe(concat('gophish.min.css'))
+        .pipe(cleanCSS({ compatibility: 'ie9' }))
+        .pipe(gulp.dest(paths.css.dest));
 }
 
-exports.vendorjs = vendorjs
-exports.scripts = scripts
-exports.styles = styles
-exports.build = gulp.parallel(vendorjs, scripts, styles)
-exports.default = exports.build
+// === Exported Tasks ===
+exports.vendorjs = vendorJS;
+exports.scripts = appScripts;
+exports.styles = styles;
+exports.build = gulp.parallel(vendorJS, appScripts, styles);
+exports.default = exports.build;
